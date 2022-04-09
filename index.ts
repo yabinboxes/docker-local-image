31ea1953b19401accfdb9f1ab7bde62c637bd5d2
yabinboxes <yabin.monroy.1@gmail.com>
2022-04-09 14:09:08 -0700

    local docker

--- index.ts
+++ index.ts
@@ -1,10 +1,47 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
+import * as docker from "@pulumi/docker";
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US"
+const imageName = "my-first-gcp-app";
+const config = new pulumi.Config();
+const codePath = config.require('projectLocation');
+
+// get configuration
+const backendPort = config.requireNumber("backend_port");
+const nodeEnvironment = config.require("node_environment");
+
+const stack = pulumi.getStack();
+
+const backendImageName = "backend";
+const backend = new docker.Image("backend", {
+    build: {
+        context: `${process.cwd()}/pulumi-backend`,
+    },
+    imageName: `${backendImageName}:${stack}`,
+    skipPush: true,
 });
 
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+// create a network!
+const network = new docker.Network("network", {
+    name: `services-${stack}`,
+});
+
+// create the backend container!
+const backendContainer = new docker.Container("backendContainer", {
+    name: `backend-${stack}`,
+    image: backend.baseImageName,
+    ports: [
+        {
+            internal: backendPort,
+            external: backendPort,
+        },
+    ],
+    envs: [
+        `NODE_ENV=${nodeEnvironment}`,
+    ],
+    networksAdvanced: [
+        {
+            name: network.name,
+        },
+    ],
+}, { dependsOn: [ ]});
